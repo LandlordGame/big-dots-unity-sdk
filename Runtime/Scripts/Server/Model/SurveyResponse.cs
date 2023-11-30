7fb57e98481b7e08ba07f17a4f8800f8c3b55b8f
tallyon <skowronski6@gmail.com>
2023-11-30 11:15:29 +0100

    survey available categories are now sourced from API instead of mock

--- Runtime/Scripts/Server/Model/SurveyResponse.cs
+++ Runtime/Scripts/Server/Model/SurveyResponse.cs
@@ -15,24 +15,6 @@ namespace SurveyAPI.Service
         /// List containing names of all possible categories that a venue can have - use for setting up category correction flow
         /// where all categories should be shown to the user.
         /// </summary>
-        public string[] PossibleCategories { get; } = new string[] {
-            "Arts and Entertainment",
-            "Business and Professional Services",
-            "Home Improvement Service",
-            "Office",
-            "Automotive Service",
-            "Community and Government",
-            "Education",
-            "Government Building",
-            "Dining and Drinking",
-            "Restaurant",
-            "Health and Medicine",
-            "Landmarks and Outdoors",
-            "Park",
-            "Retail",
-            "Food and Beverage Retail",
-            "Sports and Recreation",
-            "Travel and Transportation"
-         };
+        [JsonProperty("availableCategories")] public string[] PossibleCategories { get; set; }
     }
 }
